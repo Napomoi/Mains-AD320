198dfe9ead12e84e346210875d7e060828dcc8da
Napomoi <36967028+Napomoi@users.noreply.github.com>
2022-02-16 10:38:47 -0800

    Added  model folder and Deck,js file

---
+++ service/models/Deck.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+//Creating a schema
+
+const CardSchema = new mongoose.Schema({
+    frontImage: String,
+    frontText: String,
+    backImage: String,
+    backImage: String,
+})
+
+const DeckSchema = new mongoose.Schema({
+    name: String,
+    Cards: [CardSchema], // allows us to cretae  ana array of card objects
+    size: Number,
+    userID: mongoose.Type.ObjectID
+})
+
+export const Deck = mongoose.model('Deck', DeckSchema)
