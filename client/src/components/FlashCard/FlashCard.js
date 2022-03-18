ba2605ee0ec1212b7cfb876a6aca03609ece9110
Napomoi <36967028+Napomoi@users.noreply.github.com>
2022-03-17 17:30:16 -0700

    Setup week10 base assignment

--- client/src/components/FlashCard/FlashCard.js
+++ client/src/components/FlashCard/FlashCard.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import './FlashCard.css';
+import React from 'react'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@mui/material'
 
-function FlashCard() {
-    return <div className='card'>
-        <img src = 'https://placekitten.com/200/200'></img>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Cras blandit urna vel pellentesque luctus. Sed ut massa libero. 
-            Ut commodo, enim ac pretium ultricies, sapien lectus pellentesque ipsum,
-            vitae commodo augue est quis nulla.
-            Sed eleifend purus vitae tortor laoreet iaculis pharetra at lectus. 
-            Ut viverra felis eu augue luctus condimentum.
-            Sed nec neque nec nibh auctor pretium a non purus.
-            Nullam fermentum ex eros, ac rutrum ligula euismod ut.
-        </p>
-    </div>
+function Flashcard({ content, previous, next, flip }) {
+  return (
+    <Box sx={{ width: '100%', mt: 8, display: 'flex', justifyContent: 'space-around'}}>
+      <Card sx={{ width: '40vw' }} elevation={3}>
+        { content.image && <CardMedia
+          component="img"
+          height="200"
+          image={content.image} />}
+        <CardContent>
+          <Typography>{content.text ?? ''}</Typography>
+        </CardContent>
+        <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Button
+            sx={{ mx: 2 }}
+            onClick={ () => previous() }
+            variant="outlined"
+          >
+            Prev
+          </Button>
+          <Button sx={{ mx: 2 }} variant="outlined" onClick={() => flip()}>
+            Flip
+          </Button>
+          <Button
+            sx={{ mx: 2 }}
+            onClick={ () => next() }
+            variant="outlined"
+          >
+            Next
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  )
 }
-export default FlashCard;
+
+export default Flashcard
